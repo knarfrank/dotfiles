45f5b605fb4f1dda3ab5a0f5e10f4509ad2e24e0
knarfrank <fh13286@my.bristol.ac.uk>
2016-03-07 15:10:29 +0000

    Add aliases for grep

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,13 @@
+# General Aliases
 alias l="/bin/ls"
 alias ls="ls -oaGFh"
 alias lshome="ls ~/"
+
+alias grep="grep --color"
+alias grepr="grep -r"
+
+
+
+# Related to dotfiles and zshrc
 alias gosource="source ~/.zshrc"
 alias dots="cd ~/.dotfiles/"
