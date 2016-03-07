a498a18d4461b5a7bd3a2bbcf9f9a4a7516fead4
knarfrank <fh13286@my.bristol.ac.uk>
2016-03-07 12:59:05 +0000

    Add edit last command function

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -1,3 +1,8 @@
 # Set zsh prompt
 setopt PROMPT_SUBST
 PROMPT='%{$(pwd | sed "s/\/Users\/$(whoami)\(\/\)*/\~\//g" | grep --color=always /)%${#PWD}G%} %(!.%F{red}.%F{cyan})%n%f@%F{yellow}%m%f%(!.%F{red}.)%#%f '
+
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^E" edit-command-line
+
