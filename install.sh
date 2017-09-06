4448e1296ac864f2bb75c8aa4025c1d23448a4f2
knarfrank <fh13286@my.bristol.ac.uk>
2017-09-06 11:02:04 +0100

    Add bin directory and install script

--- install.sh
+++ install.sh
@@ -14,10 +14,19 @@ then
 	mv ~/.vimrc ~/.vimrc.backup
 fi
 
+if [ -f ~/bin ];
+then
+    mv ~/bin ~/bin.backup
+    echo "Bin file already exists in ~/bin, moving it to ~/bin.backup"
+fi
 
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/git/git.config ~/.gitconfig
 ln -s ~/.dotfiles/vim/vim.config ~/.vimrc
+ln -s ~/.dotfiles/bin ~/bin
+
+export PATH=$PATH:~/bin
+
 
 # Make zsh the default shell
 chsh -s /bin/zsh
